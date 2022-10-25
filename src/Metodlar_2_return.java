a122c0867c9fd245ba5b1e0499073b315d8c6dbf
Kkyyasin <cozdemir56.yz@gmail.com>
2022-10-25 23:35:13 +0300

    Initial commit

---
+++ src/Metodlar_2_return.java
@@ -0,0 +1,26 @@
+public class Metodlar_2_return {
+    public static void main(String[] args){
+        System.out.println("Çıktı:"+toplama(4,8));
+        System.out.println(iki_topla(kare_al(dort_carp(iki_carp(5)))));//boyle iç içe kullanabilir
+    }
+    public static int toplama(int a,int b){
+        return a+b;
+
+    }
+    public static int iki_carp(int a)
+    {
+        return 2*a;
+    }
+    public static int dort_carp(int a)
+    {
+        return 4*a;
+    }
+    public static int kare_al(int a)
+    {
+        return a*a;
+    }
+    public static int iki_topla(int a)
+    {
+        return 2+a;
+    }
+}
